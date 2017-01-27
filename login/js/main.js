0f9d94837d6d56d8ff6129b7376cc735768df310
lx3133584 <755674130@qq.com>
2017-01-27 10:57:50 +0800

    完整登录页面：初版

---
+++ login/js/main.js
@@ -0,0 +1,167 @@
+/**
+ * Created by Liang Xu on 2017/1/26.
+ */
+$(function () {
+    bindEvent();
+});
+
+/*绑定事件*/
+function bindEvent() {
+    /*tab*/
+    $(".main>h2:first-child").on("click",function () {
+        $(".main>h2").removeClass();
+        $(this).addClass("active");
+        $(".login").show();
+        $(".register").hide();
+    });
+    $(".main>h2:eq(1)").on("click",function () {
+        $(".main>h2").removeClass();
+        $(this).addClass("active");
+        $(".login").hide();
+        $(".register").show();
+    });
+    /*账号格式验证*/
+
+    /*密码格式验证*/
+    $(".main input:eq(1)").on("blur",function () {
+        var reg = /^[a-zA-Z]\w{5,17}$/;
+        var value = $(this).val();
+        if(reg.test(value)){
+            $(".main p:eq(1)").text("");
+        }
+        else{
+            $(".main p:eq(1)").text("密码包含字母、数字、下划线，以字母开头，6-18字节")
+        }
+
+    });
+    /*登录*/
+    $(".login input[type=submit]").on("click",function () {
+        event.preventDefault();
+        if($(".login p").text()==""){
+            $.ajax({
+                type:"POST",
+                url:"login.php",
+                data:{
+                    "account":$(".login input[name=account]").val(),
+                    "password":$(".login input[name=password]").val()
+                },
+                dataType:"json",
+                success:function (data) {
+                    if(data.success){
+                        $(".main h2").hide();
+                        $(".register").hide();
+                        $(".login").hide();
+                        $(".success").show();
+                        $(".success p").text("登录成功");
+                    }
+                    else {
+                        $(".login p").text(data.message);
+                    }
+
+                },
+                error:function (XHR) {
+                    alert("发生错误:"+XHR.status)
+                }
+            })
+        }
+
+    });
+    /*注册*/
+        /*用户名*/
+    $(".register input:first-child").on("blur",function () {
+        var reg = /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/;
+        var value = $(this).val();
+        if(reg.test(value)){
+            $.ajax({
+                type:"GET",
+                url:"register.php?account="+value,
+                dataType:"json",
+                success:function (data) {
+                    if(data.success){
+                        $(".register p:eq(0)").text("");
+                    }
+                    else {
+                        $(".register p:eq(0)").text(data.message);
+                    }
+
+                },
+                error:function (XHR) {
+                    alert("发生错误:"+XHR.status)
+                }
+            });
+            $(".register p:eq(0)").text("");
+        }
+        else{
+            $(".register p:eq(0)").text("用户名包含字母、数字、下划线，以字母开头，5-16字节")
+        }
+    });
+        /*密码*/
+    $(".register input:eq(1)").on("blur",function () {
+        var reg = /^[a-zA-Z]\w{5,17}$/;
+        var value = $(this).val();
+        if(reg.test(value)){
+            $(".register p:eq(1)").text("");
+        }
+        else{
+            $(".register p:eq(1)").text("密码包含字母、数字、下划线，以字母开头，6-18字节")
+        }
+    });
+        /*确认密码*/
+    $(".register input:eq(2)").on("blur",function () {
+        var previous_value = $(".register input:eq(1)").val();
+        var value = $(this).val();
+        if(value == previous_value){
+            $(".register p:eq(2)").text("");
+        }
+        else{
+            $(".register p:eq(2)").text("两次密码输入不一致，请重新输入")
+        }
+    });
+        /*注册成功*/
+    $(".register input[type=submit]").on("click",function () {
+        event.preventDefault();
+        $(".register input:first-child").blur();
+        $(".register input:eq(1)").blur();
+        $(".register input:eq(2)").blur();
+        if($(".register p").text()==""){
+            $.ajax({
+                type:"POST",
+                url:"register.php",
+                data:{
+                    "account":$(".register input[name=account]").val(),
+                    "password":$(".register input[name=password]").val(),
+                    "name":$(".register input[name=name]").val(),
+                    "sex":$(".register input[name=sex]").val()
+                },
+                dataType:"json",
+                success:function (data) {
+                    $(".main h2").hide();
+                    $(".register").hide();
+                    $(".login").hide();
+                    $(".success").show();
+                    if(data.success){
+                        $(".success p").text("注册成功");
+                    }
+                    else {
+                        $(".success p").text(data.message);
+                    }
+
+                },
+                error:function (XHR) {
+                    alert("发生错误:"+XHR.status)
+                }
+            })
+        }
+
+    });
+            /*返回键*/
+    $(".success input[type=button]").on("click",function () {
+        $(".success").hide();
+        $(".main h2").show();
+        $(".main>h2:first-child").click();
+    })
+
+}
+
+
+
---
+++ login/login.php
@@ -0,0 +1,45 @@
+<?php
+
+$servername = "bdm259768380.my3w.com";
+$username = "bdm259768380";
+$password = "w0shilxo";
+$dbname = "bdm259768380_db";
+
+// 创建连接
+$con = mysql_connect($servername,$username,$password);
+// 检测连接
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+ mysql_select_db($dbname, $con);
+
+//判断如果是get请求，则进行搜索；如果是POST请求，则进行新建
+//$_SERVER是一个超全局变量，在一个脚本的全部作用域中都可用，不用使用global关键字
+//$_SERVER["REQUEST_METHOD"]返回访问页面使用的请求方法
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+	echo Error METHOD
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST"){
+	search();
+}
+
+function search(){
+	$account = $_POST["account"];
+	$password = $_POST["password"];
+	$sql="SELECT account FROM  MyGuests WHERE (account='$account') AND (password='$password') ";
+	$query=mysql_query($sql);
+	$rows=mysql_num_rows($query); 
+
+	if($rows>0){
+		$row = mysql_fetch_array($query);
+		echo '{"success":true,"message":"登陆成功","name":$row['name'],"sex":$row['sex']}';
+	}
+	else{
+		echo '{"success":false,"message":"用户名或密码不正确"}';
+	}
+}
+
+
+
+?>
